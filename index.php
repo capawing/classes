31e656588ed59341662df1191437105f7a2bc12d
capawing <35475195+capawing@users.noreply.github.com>
2019-03-06 11:02:43 -0800

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
 <img src="Class Diagram (1).jpg">
 <p>In these class diagram it specify the rules of classes candidate.
 <br>The table Position is the main entrance of all the User transactions like RIDERS,DRIVER AND ADMIN.
-<br>Riders table where the communters can log in there personal information before they can use thies application processes.</br>
+<br>Riders table where the communters can log in there personal information before they can use thies application processes.
 <br>Payments table are connected with the passengers transactions,which help to guide where is the exact route and equivalent fare
-<br> that they need to paid for.before the time of departure and arrival.<br>The fare guide table are very useful information which 
-  we can find <br>it here.the selection methods and payments methods,which make us hassle free, safe and convient.
+<br> that they need to paid for.before the time of departure and arrival.The fare guide table are very useful information which 
+  we can find it here,the selection methods and payments methods,which make us hassle free, safe and convient.
 <br>Driver table it contains the personal information about the Driver,where they can login and star monitoring about 
-<br>the passenger status,if they already verifying their ticket or not.<br>the Admin management personnel he /she is the Authorized 
-  personnel to add user, update DSR table and can view the drivers ratings.</br> and verify riders tickets.</p>
+<br>the passenger status,if they already verifying their ticket or not.the Admin management personnel he /she is the Authorized 
+  personnel to add user, update DSR table and can view the drivers ratings and verify riders tickets.</br></p>
 
 </body>
 </html>
