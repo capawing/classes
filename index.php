c8af488d353cb1035f65c6e838b8fe8e46756589
capawing <35475195+capawing@users.noreply.github.com>
2019-03-06 10:45:07 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title></title>
+<title>Class Diagram</title>
 </head>
 <body>
-<img src="Class Diagram.jpg">
+<img src="Class Diagram (1).jpg">
 <p>In these class diagram it specify the rules of classes candidate.</p>
-<p>The table Posistion is the main entrance of all the User transactions like PASSENGER,DRIVER/CONDUCTOR AND ADMIN.
-<br>Passenger table where the communters can log in there personal information before they can use thies application processes.</br>
+<p>The table Posistion is the main entrance of all the User transactions like RIDERS,DRIVER AND ADMIN.
+<br>Riders table where the communters can log in there personal information before they can use thies application processes.</br>
 <br>Payments table are connected with the passengers transactions,which help to guide where is the exact route and equivalent fare
-<br> that they need to paid for.before the time of arrival.<br>The route guide table are very useful information which we can find 
-<br>it here.the route destination,Busname,Bus codenumber,which make us hassle free, safe and convient.
-<br>Driver/Conductor table it contains the personal information about the Driver,where they can login and star monitoring about 
-<br>the passenger status,if they paid already or not.<br>The D_SR table is connected through the entire transactions from the driver,
-<br>where he can update or report of how many passengers he made and whole day trips income.which very import to report it into 
-<br>the Admin management personnel.since the Admin is the Authorized personnel to some up all the income into the DSR table.</br></p>
+<br> that they need to paid for.before the time of departure and arrival.<br>The fare guide table are very useful information which 
+  we can find <br>it here.the selection methods and payments methods,which make us hassle free, safe and convient.
+<br>Driver table it contains the personal information about the Driver,where they can login and star monitoring about 
+<br>the passenger status,if they already verifying their ticket or not.<br>the Admin management personnel he /she is the Authorized 
+  personnel to add user, update DSR table and can view the drivers ratings.</br> and verify riders tickets.</p>
 
 </body>
 </html>
