5073cd6ce8f4621704e39487f6b05139323b07e0
capawing <35475195+capawing@users.noreply.github.com>
2019-03-06 11:07:11 -0800

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 <br>Riders table where the communters can log in there personal information before they can use thies application processes.
 <br>Payments table are connected with the passengers transactions,which help to guide where is the exact route and equivalent fare
 <br> that they need to paid for.before the time of departure and arrival.The fare guide table are very useful information which 
-  we can find it here,the selection methods and payments methods,which make us hassle free, safe and convient.
+  we can find it here,<br>the selection methods and payments methods,which make us hassle free, safe and convient.
 <br>Driver table it contains the personal information about the Driver,where they can login and star monitoring about 
-<br>the passenger status,if they already verifying their ticket or not.the Admin management personnel he /she is the Authorized 
+the passenger status,<br>if they already verifying their ticket or not.the Admin management personnel he /she is the Authorized 
   personnel to add user, update DSR table and can view the drivers ratings and verify riders tickets.</br></p>
 
 </body>
